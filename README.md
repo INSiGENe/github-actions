6114059608f1c93df360fc998125a62dd2232cb2
Seb Rauschert <sebastian.rauschert@insigene.com>
2024-10-21 10:07:46 +0800

    Added a seurat example

---
+++ README.md
@@ -0,0 +1,10 @@
+# GitHub Actions Example
+
+This repository demonstrates an example of GitHub actions. The workflow is triggered on every push to the repository. 
+
+## Workflow 1
+A Simple R analysis, generating a random data set, and plotting it with ggplot2. We then save the data as a csv and the plot as a png.
+
+## Workflow 2
+As Bioinformatics is at the core of INSiGENe's business, we also demonstrate running at least part of the standard `Seurat` analysis pipeline via GitHub 
+Actions. This of course will not be feasible for (a) data with ethics concerns (b) larger data sets. 
---
+++ scripts/seurat.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(readr)
+library(ggplot2)
+
+# 1. Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "data/filtered_gene_bc_matrices/hg19/")
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+
+# The [[ operator can add columns to object metadata. This is a great place to stash QC stats
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+# 2. Visualize QC metrics as a violin plot
+violin <- VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+# 3. FeatureScatter is typically used to visualize feature-feature relationships, but can be used
+# for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
+
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+feature_plot <- plot1 + plot2
+
+print("Save feature plot")
+ggsave("results/feature_plot.png", feature_plot, width = 8, height = 6)
+
+# 4. Normalize the data
+pbmc <- NormalizeData(pbmc)
+
+
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+
+
+# 5. Plot variable features with and without labels
+plot1 <- VariableFeaturePlot(pbmc)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+variable_features <- plot1 + plot2
+
+print("Save variable features plot")
+ggsave("results/variable_features.png", variable_features, width = 12, height = 6)
